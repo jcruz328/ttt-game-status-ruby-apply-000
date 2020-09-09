a4c6e2a22d0308ef24ed14b9857db66b55fbd953
Joel <jcruz328@gmail.com>
2020-09-09 03:57:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,7 +39,10 @@ WIN_COMBINATIONS = [
     won?(board) || draw?(board) || full?(board)
   end
   
-  def winner
-    
+  def winner(board)
+    WIN_COMBINATIONS.each do |arr|
+      if (board[arr[0]] == "X" && board[arr[1]] == "X" && board[arr[2]] == "X") || (board[arr[0]] == "O" && board[arr[1]] == "O" && board[arr[2]] == "O")
+        return arr[0]
+      end
   end
   
